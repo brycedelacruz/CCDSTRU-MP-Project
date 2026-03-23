170c9a475829fc1e71fdb5ca9feee768a1f97e72
ISDC-08 <is.chionglo.05@gmail.com>
2026-03-23 18:13:05 +0800

    Update DISTRU_MP_Header.h

--- DISTRU_MP_Header.h
+++ DISTRU_MP_Header.h
@@ -1,1 +1,18 @@
+/*
+  This File Contains All The Definitions And Function Declarations
+*/
 
+// Defining Applicable Sets And Initialization
+typedef int M[3][3];
+
+// Defining System Global Variables
+int good = 0, go = 1, start = 1, over = 0, found = 0, val = 0;
+M R, B, S, T, F;
+
+// Game Function Declarations
+void clear(M pos);
+void replace(M pos);
+void expand(M pos);
+void update(M pos);
+void nextplayermove(M pos);
+void gameover();
